8cff7a06f1bdf23037679ddab29605b27aa177ba
nagarajanvijay69 <nagarajanvijay69@gmail.com>
2025-05-14 13:52:40 +0530

    s

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import cron from 'node-cron';
 const app = express();
 const PORT = 5000;
 app.use(cors({
-     origin: "http://localhost:5173",
+     origin: "https://loan-app-frontend-opal.vercel.app/",
      credentials: true,
 }));
 app.use(express.json());
